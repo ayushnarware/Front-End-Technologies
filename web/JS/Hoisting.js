132747508458f8b5cd9c3aab58927a5b4adf75af
Ayush Narware <152053797+ayushnarware@users.noreply.github.com>
2025-10-14 14:50:58 +0530

    Add Hoisting

---
+++ web/JS/Hoisting.js
@@ -0,0 +1,18 @@
+    let sum = add(5, 10); // sum is assigned the result of add(5, 10)
+
+    function add(a, b) {
+        return a + b;
+    }
+    
+    
+    console.log(value); // ❌ ReferenceError
+    let value = 10;
+    
+    
+    let sum2 = add(5, 10); // function call
+    function add(a, b) {
+        return a + b;
+    }
+    console.log(sum2); // ✅ Output: 15
+
+
