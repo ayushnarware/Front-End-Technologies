8c180d4d0d8986888a96360ae7e61e6825115bb1
Ayush Narware <152053797+ayushnarware@users.noreply.github.com>
2025-10-28 14:50:41 +0530

    events

---
+++ web/JS/Hof.js
@@ -0,0 +1,28 @@
+// script.js
+
+// Console me simple message print karna
+console.log("Hello Ayush! 👋 Welcome to JavaScript Console Example.");
+
+// Variables
+let name = "Ayush Narware";
+let age = 21;
+
+// Console me variables print karna
+console.log("Name:", name);
+console.log("Age:", age);
+
+// Math operation
+let sum = 10 + 20;
+console.log("Sum of 10 + 20 =", sum);
+
+// Condition check
+if (age > 18) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are not an adult yet.");
+}
+
+// Loop Example
+for (let i = 1; i <= 5; i++) {
+  console.log("Number:", i);
+}
