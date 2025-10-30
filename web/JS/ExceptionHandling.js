e8268b3dfd490232cc155ad6d612043d362dd340
Ayush Narware <152053797+ayushnarware@users.noreply.github.com>
2025-10-30 15:22:24 +0530

    Promises file add

--- web/JS/ExceptionHandling.js
+++ web/JS/ExceptionHandling.js
@@ -4,6 +4,7 @@ let b = 0;
 
 try {
     console.log(a / b );
+    c=87;
 }
 catch (e) {
     console.log("this is error");
@@ -11,12 +12,40 @@ catch (e) {
 
 
 
+// console.log("start");
+// try {
+//   // Code that might throw an error
+//   let num = 10;
+//   let result = num/ x;
+//   console.log(result);
+  
+// }
+// catch(e){
+//   console.log("The custom error", e.message);
+// } 
+  
+// finally {
+// console.log("end");
+// }
+  
+
+console.log("start");
 
 try {
-  setTimeout(() => {
-    throw new Error("Async error"); // ❌ won't be caught
-  }, 1000);
-} catch (e) {
-  console.log("Caught:", e); // ❌ won't run
+  let num = 10;
+  let x = 0;
+
+  if (x === 0) {
+    // manually throw a custom error
+    throw new Error("Division by zero is not allowed!");
+  }
+
+  let result = num / x;
+  console.log(result);
 }
-setTimeout();
+catch (e) {
+  console.log("Custom Error:", e.message);
+}
+finally {
+  console.log("end");
+}
